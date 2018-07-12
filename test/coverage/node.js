965123d79257c630257e0cb6206d9a64d95c151e
Andrew Plummer <plummer.andrew@gmail.com>
2018-07-12 22:44:36 +0900

    Updated nyc options and removed old references to fold mode.

--- test/coverage/node.js
+++ test/coverage/node.js
@@ -11,19 +11,18 @@ function run(cmd) {
 
 console.info('Running all tests...');
 
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest -r test/index.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest --no-globals test/tests/standalone/import.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest --no-globals test/tests/standalone/require.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest test/tests/standalone/utils.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest test/tests/standalone/focused-suite.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest test/tests/standalone/focused-test.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest test/tests/standalone/empty.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest -f top test/tests/suites.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest -f none test/tests/suites.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest test/tests/standalone/failure-suite.js');
-run('./node_modules/nyc/bin/nyc.js -s --no-clean bin/readytest test/tests/standalone/failure-test.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest -r test/index.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest --no-globals test/tests/standalone/import.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest --no-globals test/tests/standalone/require.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest test/tests/standalone/utils.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest test/tests/standalone/focused-suite.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest test/tests/standalone/focused-test.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest test/tests/standalone/empty.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest -f top test/tests/suites.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest -f none test/tests/suites.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest test/tests/standalone/failure-suite.js');
+run('./node_modules/nyc/bin/nyc.js bin/readytest test/tests/standalone/failure-test.js');
 
 // Absolute paths to relative
 var path = process.cwd().replace(/\//g, '\\/');
 run(`sed -i '' -e "s/${path}\\///g" .nyc_output/*`);
-
