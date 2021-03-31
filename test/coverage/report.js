e55f158221f174059e2d9f34246e222151a2c8e9
Andrew Plummer <plummer.andrew@gmail.com>
2021-03-31 16:19:42 +0900

    removed coverage and deps for now

--- test/coverage/report.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const { run, open } = require('./helpers');
-
-console.info('Generating report...');
-run('./node_modules/.bin/nyc report');
-open('./test/coverage/output/report/index.html');
-console.info('Done!');
